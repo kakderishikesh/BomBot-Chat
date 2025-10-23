00cc44cfd2692c9605bd6df1f64b9fe2ab702221
kakderishikesh <rishikesh.kakde59@gmail.com>
2025-10-22 20:34:46 -0400

    feat: vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import { ChatProvider } from "./contexts/ChatContext";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
@@ -30,6 +31,7 @@ const App = () => (
             </Routes>
           </Layout>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </ChatProvider>
   </QueryClientProvider>
